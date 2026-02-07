8fdf3dac43dca0c91247f20191beb38632284001
sarvarbeksoporboyev8-debug <sarvarbeksoporboyev8@gmail.com>
2026-02-08 02:23:00 +0500

    Create main React component for App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello, Numeo Task!</h1>
+            <p>This is the main React component.</p>
+        </div>
+    );
+};
+
+export default App;
